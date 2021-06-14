fa2a189c02edf31f4e795bcd4d5ec89571707d17
abhinavmehta257 <61967812+abhinavmehta257@users.noreply.github.com>
2021-06-14 13:10:23 +0530

    first commit

---
+++ starbase/src/App.vue
@@ -0,0 +1,34 @@
+<template >
+  <div id="app">
+    <h1 align='center'>{{title}}</h1>
+    <div class="container">
+      <button class="btn btn-outline-success col-md-12" name="button" @click='addNewMember'>Add New Member</button>
+      <div class="row">
+        <character v-for='id in initial_ids' :id='id'/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script type="text/javascript">
+  import character from './components/charecter.vue'
+  export default{
+    name: 'app',
+    data(){
+      return{
+        title: 'Generate Your Team',
+        initial_ids: [1, 13, 14]
+      }
+    },
+    methods:{
+      addNewMember(){
+        var new_id = Math.floor(Math.random()*81)+1
+        this.initial_ids.push(new_id)
+      }
+    },
+    components:{
+      character
+    }
+  }
+</script>
---
+++ starbase/src/components/charecter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-md-4" @click='switchCharacter'>
+    <div class="character-card mt-3">
+      <div class="card-block">
+        <h4 class="card-title">{{character.name}}</h4>
+        <p class="card-text">Height: {{character.height}}</p>
+        <p class="card-text">Mass: {{character.mass}}</p>
+        <p class="card-text">Hair colour: {{character.hair_color}}</p>
+        <p class="card-text">Eye Color: {{character.eye_color}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+  export default{
+    props:['id'],
+    data(){
+      return{
+        character:{}
+      }
+    },
+    methods:{
+      fetchCharecter(id){
+        fetch('https://swapi.dev/api/people/'+id, {
+          method:'GET'
+        }).then(response => response.json())
+        .then(json => this.character = json)
+      },
+      switchCharacter(){
+        let random_id = Math.floor(Math.random()*81)+1
+        this.fetchCharecter(random_id)
+      },
+      removeCharacter(){
+        return(this.id)
+      }
+    },
+    created(){
+      this.fetchCharecter(this.id)
+    }
+
+  }
+</script>
